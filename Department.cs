94f3fd016b2e662e8effa7c11dbbb5faab0afd32
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-29 16:13:22 +0200

    Add project files.

---
+++ Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_App_Using_REST_APIs
+{
+    internal class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Desktop_App_API
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
